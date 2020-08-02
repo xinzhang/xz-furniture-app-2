6c7f18b96e92781a94814e4bc84c4f0fc8fd5b04
Xin Zhang <xinzhang@192-168-1-125.tpgi.com.au>
2020-08-02 23:07:33 +1000

    init screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:xz_furniture_app_2/size_config.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold();
+  }
+}
